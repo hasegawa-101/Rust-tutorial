9f1d9790c4861b7c4b70f6d9f1c4175f373ffe60
hasegawa-101 <hasegawa@101de-sign.com>
2022-01-01 23:32:01 +0900

    calc test

--- calculation/src/main.rs
+++ calculation/src/main.rs
@@ -1,5 +1,10 @@
+use std::io;
 mod calc;
 
 fn main() {
-    println!("{}",calc::calc(2,2));
+    println!("num1を入力してください。");
+    let mut num1_string = String::new();
+    io::stdin().read_line(&mut num1_string).expect("Failed to read line");
+    let num1: i32 = num1_string.parse().unwrap();
+    println!("{}",calc::calc(num1,2));
 }
