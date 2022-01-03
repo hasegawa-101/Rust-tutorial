52202080ffec25ba4b065e06acac0e1f211e0815
hasegawa-101 <hasegawa@101de-sign.com>
2022-01-03 23:40:42 +0900

    thread

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,3 +1,10 @@
+use std::thread;
+use std::thread::JoinHandle;
+
 fn main() {
-    println!("Hello, world!");
+    let handle:JoinHandle<()> = thread::spawn(|| {
+        println!("Hello, from thread.");
+    });
+
+    handle.join().unwrap();
 }
